7c560876dda08742c4ff75928cb905fe4c96d6c8
fighteros <am5419@fayoum.edu.eg>
2021-03-29 21:30:53 +0200

    fixed portges problem and db connected successfully

--- src/main/java/com/example/demo/datasource/PostgresDataSource.java
+++ src/main/java/com/example/demo/datasource/PostgresDataSource.java
@@ -7,8 +7,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class postgresDataSource {
-
+public class PostgresDataSource {
     @Bean
     @ConfigurationProperties("app.datasource")
     public HikariDataSource hikariDataSource() {
